715f7d1609aa8eafd030f18efc538754c7cbdac8
Nikita <mikita.pankevich00@gmail.com>
2020-02-09 11:52:47 +0300

    Upload project

---
+++ app/src/main/java/com/demo/cryptocompareapp/database/CoinPriceInfoDao.kt
@@ -0,0 +1,20 @@
+package com.demo.cryptocompareapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.demo.cryptocompareapp.POJO.CoinPriceInfo
+
+@Dao
+interface CoinPriceInfoDao {
+    @Query("SELECT * FROM full_price_list ORDER BY lastUpdate DESC")
+    fun getPriceList():LiveData<List<CoinPriceInfo>>
+
+    @Query("SELECT * FROM full_price_list WHERE fromSymbol ==:fromSymbol LIMIT 1")
+    fun getPriceInfoAboutCoin(fromSymbol: String): LiveData<CoinPriceInfo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPriceList(priceList: List<CoinPriceInfo>)
+}
